1730906f41b56b452b8f80b2a4361acec76e3de0
vegardinho <landsverk.vegard@gmail.com>
2020-03-23 21:08:29 +0100

    Write json to file on run without argument; read from file on run with arguments. Faster filtering.

--- return_device_json.py
+++ return_device_json.py
@@ -2,13 +2,20 @@ from subprocess import Popen, PIPE
 import json
 import sys
 
-# Open pipe to bash, run command, read output, and convert json to list.
-p = Popen(["/usr/local/bin/blueutil", "--paired", "--format" ,"json"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-devices = json.loads(output.decode('utf-8'))
 
 subtitle = sys.argv[1]
 num_arg = len(sys.argv)
+
+if num_arg == 2:
+# Open pipe to bash, run command, read output, and convert json to list.
+    p = Popen(["/usr/local/bin/blueutil", "--paired", "--format" ,"json"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    text_json = output.decode('utf-8')
+    devices = json.loads(text_json)
+
+    open("devices.json", "w").write(text_json)
+else:
+    devices = json.loads(open("devices.json", "r").read())
 
 # Create json manually from content in list (and argument), and print to alfred
 items = """{"items": ["""
