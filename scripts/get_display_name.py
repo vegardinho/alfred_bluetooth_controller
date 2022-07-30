b50dd316b7d7ce2e7ad1d2257ff80ce2dfb7c1b4
vegardinho <landsverk.vegard@gmail.com>
2022-07-31 00:40:39 +0200

    Cleanup

--- scripts/get_display_name.py
+++ scripts/get_display_name.py
@@ -3,17 +3,26 @@
 from subprocess import check_output
 import json
 import sys
+import xml.etree.ElementTree as ET
 
-from return_device_json import get_display_name
 
-
-def main():
-	dev_id = sys.argv[1]
-	dev_name = get_display_name(dev_id)
+def get_display_name(dev_id):
+	dev_name = get_custom_name(dev_id)
 	if not dev_name:
 		dev_info = check_output(["./blueutil", "--format", "json", "--info", dev_id])
 		dev_name = json.loads(dev_info)["name"]
-	sys.stdout.write(dev_name)
+	return dev_name
+
+
+def get_custom_name(address):
+	"""Check cache file for custom file name"""
+	try:
+		plist_cmd = "plutil -extract DeviceCache.{}.displayName xml1 /Library/Preferences/com.apple.Bluetooth.plist -o -".format(address)
+		xml = check_output(plist_cmd.split())
+	except Exception as e:
+		return None
+	return ET.fromstring(xml).find('string').text
+
 
 if __name__ == "__main__":
-    main()
+	get_display_name(sys.argv[1])
