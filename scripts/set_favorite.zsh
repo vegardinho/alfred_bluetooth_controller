33764002068277f1fac3fcbe5e21e1954d2e48c2
vegardinho <landsverk.vegard@gmail.com>
2022-08-11 16:42:15 +0200

    Correct suggested commands in notification

--- scripts/set_favorite.zsh
+++ scripts/set_favorite.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
 security add-generic-password -a "alfred-bluetooth-device-id" -s "alfred-bluetooth-device-id" -w "$uid" -C "note" -U
-./notificator --title "Favorite device set to $device_name" --message 'Use "btx" to connect to this device directly' --sound 'Frog'
+./notificator --title "Favorite device set to $device_name" --message 'Use keyword "btf" or hotkey "cmd-ctrl-alt-f" to connect to this device directly' --sound 'Frog'
+
